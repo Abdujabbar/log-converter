54dab15168986b33398b6a3a2eb5ec7b2dc42fc5
Abdujabbor <abdujabbor1987@gmail.com>
2018-08-15 17:51:55 +0500

    modifications

--- main.go
+++ main.go
@@ -22,7 +22,23 @@ var dao = repository.DAO{
 var tableTmpl = "<table border=1 cellpadding=10 cellspacing=5 style='width: 100%%'><thead><th>ID</th><th>TIME</th><th>MSG</th><th>FORMAT</th></thead><tbody>%s</tbody></table>"
 var rawTmpl = "<tr><td>%v</td><td>%v</td><td>%v</td><td>%v</td></tr>"
 
-func serveLogsList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func main() {
+	fmt.Println("Please wait until servers will be ready")
+	err := dao.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	go process(os.Args)
+
+	router := httprouter.New()
+
+	router.GET("/logs/:page", logsHandler)
+
+	http.ListenAndServe(":8080", router)
+}
+
+func logsHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	page := params.ByName("page")
 	currentPage := 1
 	if v, err := strconv.Atoi(page); err == nil {
@@ -43,20 +59,4 @@ func serveLogsList(w http.ResponseWriter, r *http.Request, params httprouter.Par
 	}
 	table := fmt.Sprintf(tableTmpl, tableBody)
 	w.Write([]byte(table))
-}
-
-func main() {
-	fmt.Println("Please wait until servers will be ready")
-	err := dao.Connect()
-	if err != nil {
-		panic(err)
-	}
-
-	go process(os.Args)
-
-	router := httprouter.New()
-
-	router.GET("/list/:page", serveLogsList)
-
-	http.ListenAndServe(":8080", router)
 }
